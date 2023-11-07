26a3f79c83ee7a8bee0e3efb3295936687fe8b24
Choe <ceminh97@naver.com>
2023-11-07 11:35:38 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const initialstate = [
+    {
+      id: 1,
+      title: "리액트",
+      content: "리액트 공부하기",
+      isDone: false,
+    },
+    {
+      id: 2,
+      title: "리액트",
+      content: "리액트 공부하기",
+      isDone: false,
+    },
+  ];
+
+  const [todo, setTodo] = useState(initialstate);
+
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      제목
+      <input
+        value={title}
+        onChange={(event) => {
+          setTitle(event.target.value);
+        }}
+      />
+      내용
+      <input
+        value={content}
+        onChange={(event) => {
+          setContent(event.target.value);
+        }}
+      />
+      <button
+        onClick={() => {
+          const newTodo = { ...todo };
+        }}
+      >
+        추가하기
+      </button>
+      <div>
+        {todo.map(function (item) {
+          return (
+            <>
+              <div className="working">
+                <h1>{item.id}</h1>
+                <h1>{item.title}</h1>
+                <h2>{item.content}</h2>
+                <h5>{item.isDone}</h5>
+              </div>
+            </>
+          );
+        })}
+      </div>
     </div>
   );
 }
